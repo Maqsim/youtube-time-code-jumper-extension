a64a9262b0bdba392f7cad28db80c77454df10f4
Max Diachenko <maqsim@mail.com>
2023-03-15 14:16:35 +0200

    improve extension; fix bugs

--- source/content.js
+++ source/content.js
@@ -1,44 +1,61 @@
 (async () => {
-  let videoPlayer;
+  console.log('init');
+  let videoPlayerEl; // YouTube player element
+  let controlsEl; // Player controls area element
   let lastScrollPosition;
   let lastCommentEl;
+  let wasPaused;
 
   // Utility variables
   let ignoreSeekingCallback;
+  let ignoreSrcChangeCallback;
   const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-  // Wait until YouTube player is initialized
+  // Initialization
+  // ==============
+
+  // Wait until YouTube player and controls elements are initialized
   await new Promise(resolve => {
     const interval = setInterval(() => {
-      videoPlayer = document.querySelector('video.html5-main-video')
-      if (videoPlayer) {
+      videoPlayerEl = document.querySelector('video.html5-main-video');
+      controlsEl = document.querySelector('#ytd-player .ytp-chrome-bottom');
+
+      if (videoPlayerEl && controlsEl) {
         clearInterval(interval);
         resolve();
       }
     }, 100);
   });
 
-  // Create 'back to comments' button
+  // Create 'Back to comments' button
   const backToCommentButton = document.createElement('div');
   backToCommentButton.textContent = 'Back to comments';
   backToCommentButton.classList.add('ytp-back-to-comment-button', 'ytp-button');
   backToCommentButton.hidden = true;
 
-  // If Safari browser add class to avoid showing the button on full screen
+  // If Safari browser add class to avoid showing the button on full-screen (scroll doesn't work on Safari in full-screen)
   if (isSafari) {
     backToCommentButton.classList.add('is-safari');
   }
 
-  // Insert it to player's controls area
-  document.querySelector('.ytp-chrome-bottom').prepend(backToCommentButton);
+  // Insert it to main YouTube player's controls area
+  controlsEl.prepend(backToCommentButton);
 
-  // When backToCommentButton click scroll to lastScrollPosition
+  // Event handlers
+  // ==============
+
+  // When backToCommentsButton is clicked scroll to lastScrollPosition
   backToCommentButton.addEventListener('click', () => {
     if (lastScrollPosition) {
       getScrollable().scrollTo(0, lastScrollPosition);
 
+      // If video was paused before clicking time code – pause it
+      if (wasPaused) {
+        videoPlayerEl.pause();
+      }
+
       // Save reference link to another variable
-      // Highlight smoothly lastCommentEl element
+      // Smoothly highlight lastCommentEl element
       const _lastCommentRef = lastCommentEl;
       setTimeout(() => {
         _lastCommentRef.classList.add('highlight');
@@ -52,13 +69,13 @@
     }
   });
 
-  // Clicking on timecode link...
-  // Mouseup, cuz click is prevented by YouTube
+  // Mouseup here, because 'click' is prevented by YouTube
   document.addEventListener('mouseup', (e) => {
     const target = e.target;
 
-    // Check if target is A and have class yt-simple-endpoint and href attribute
+    // Clicking time code link...
     if (target.tagName === 'A' && target.classList.contains('yt-simple-endpoint') && target.hasAttribute('href')) {
+      // Check if link has time code
       const isTimeCode = Boolean(target.getAttribute('href')?.split('t=')[1]);
       if (isTimeCode) {
         ignoreSeekingCallback = true;
@@ -66,16 +83,30 @@
         lastScrollPosition = getScrollPosition();
         lastCommentEl = target.closest('ytd-comment-renderer');
         backToCommentButton.title = `Back to comment "${ lastCommentEl.querySelector('#content-text').textContent }"`;
+        wasPaused = videoPlayerEl.paused;
       }
+    }
+
+    // Clicking ad skip button...
+    if (target.closest('.ytp-ad-skip-button')) {
+      ignoreSrcChangeCallback = true;
     }
   });
 
-  // Detect video source change and reset the state
-  const observer = new MutationObserver(reset);
-  observer.observe(videoPlayer, { attributes: true, attributeFilter: ['src'] });
+  // Detect video source change (ad skip, play another video)
+  const observer = new MutationObserver(() => {
+    // When ad is skipped – ignore it
+    if (ignoreSrcChangeCallback) {
+      ignoreSrcChangeCallback = false;
+      return;
+    }
 
-  // Detect user seeking video and reset the state
-  videoPlayer.addEventListener('seeking', () => {
+    reset();
+  });
+  observer.observe(videoPlayerEl, { attributes: true, attributeFilter: ['src'] });
+
+  // Detect user seeking video
+  videoPlayerEl.addEventListener('seeking', () => {
     // First seeking is happening by clicking on time code – ignore it
     if (ignoreSeekingCallback) {
       ignoreSeekingCallback = false;
@@ -85,17 +116,22 @@
     reset();
   });
 
+  // Helper functions
+  // ================
+
   function reset() {
     ignoreSeekingCallback = false;
+    ignoreSrcChangeCallback = false;
     backToCommentButton.hidden = true;
     lastScrollPosition = null;
     lastCommentEl = null;
+    wasPaused = null;
   }
 
   function getScrollable() {
     const ytdAppEl = document.querySelector('ytd-app');
-    // Detect whether YouTube player is full screen or not
     const isFullScreen = ytdAppEl.hasAttribute('scrolling');
+
     return isFullScreen ? ytdAppEl : window;
   }
 
